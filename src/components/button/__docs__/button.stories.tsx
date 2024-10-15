c201a5a2f5e139daf82734ef2eb2a0940fbc8a47
laurarcc <laurargct@gmail.com>
2024-10-15 11:31:39 +0100

    milibreria

---
+++ src/components/button/__docs__/button.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Example from "./example"; //importamos el componente Example
+const meta: Meta<typeof Example> = {
+    title: "CustomButton",
+    component: Example,
+};
+export default meta;
+type Story = StoryObj<typeof Example>;
+export const Primary: Story = {
+    args: {
+        text: "Button primary",
+        txtcolor: 'black',
+        bgcolor: 'red',
+        disabled: false,
+        size: "small",
+        onClick: () => alert("Button Primary"),
+    },
+};
+export const Secondary: Story = {
+    args: {
+        text: "Button secondary",
+        txtcolor: 'white',
+        bgcolor: 'pink',
+        disabled: false,
+        size: "small",
+        onClick: () => alert("Button secondary"),
+    },
+};
---
+++ src/components/button/__docs__/example.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+//importamos nuestro componente CustomButton y sus props
+import CustomButton, { CustomButtonProps } from "../button";
+//Creamos un componente Example al cual le pasaremos las props definidas
+//en nuestro componente CustomButton. Luego renderizamos nuestro componente
+//<CustomButton /> con esas props. La idea es que llamemos al componente Example
+//con valores de las props para renderizar el componente <CustomButton /> de diferentes
+//maneras, es decir, crear una historia de renderizados de nusetro componente.
+function Example(props: CustomButtonProps){
+    return(
+        <CustomButton
+            text={props.text}
+            bgcolor={props.bgcolor}
+            txtcolor={props.txtcolor}
+            disabled={props.disabled}
+            size={props.size}
+            onClick={props.onClick}
+        />
+    )
+}
+export default Example
